6430fd0ffbde69f8a85d69a911fc95bd78c27c37
blala69 <1114756045@qq.com>
2020-07-28 15:35:54 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
+    <MainTabbar></MainTabbar>
   </div>
 </template>
 
 <script>
+import MainTabbar from 'components/content/mainTabbar/MainTabbar'
+
 export default {
   name:'App',
   components:{
-
+    MainTabbar,
   }
+  
 }
 </script>
 
 <style>
-  @import "./assets/css/base.css"
+  @import url("assets/css/base.css");
 </style>
